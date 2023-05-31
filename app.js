e579dd799e93fde0b78affdf9eb8cf3d9c7a50e6
TemitayoKuyoro <104427546+TemitayoKuyoro@users.noreply.github.com>
2023-05-31 17:04:43 +0100

    connected to frontend

--- app.js
+++ app.js
@@ -6,11 +6,21 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/", feedRoutes);
 
 mongoose
   .connect(
-    "mongodb+srv://tayo28kuyoro:YaiMWk31cssO06Et@cluster0.bsvcyyj.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://tayo28kuyoro:YaiMWk31cssO06Et@cluster0.bsvcyyj.mongodb.net/news?retryWrites=true&w=majority"
   )
   .then((result) => {
     console.log(result);
--- controllers/feed.js
+++ controllers/feed.js
@@ -2,13 +2,42 @@ const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
   Post.find()
-    .then((post) => console.log(post))
-    .catch((err) => console.log(err));
+    .then((posts) => {
+      res
+        .status(200)
+        .json({ message: "Fetched posts successfully", posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.getPost = (req, res, next) => {
   const newsId = req.params.newsId;
   Post.findById(newsId)
-    .then((post) => console.log(post))
-    .catch((err) => console.log(err));
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({
+        message: "Post fetched",
+        post: {
+          id: post._id,
+          title: post.title,
+          image: post.image,
+          description: post.description,
+        },
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
